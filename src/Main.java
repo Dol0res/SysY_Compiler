94b3fbff86fdd6045b0e9a7c044bf47a825e3786
qb <221250067@smail.nju.edu.cn>
2024-03-03 10:37:02 +0800

    0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        String filePath = args[0];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
