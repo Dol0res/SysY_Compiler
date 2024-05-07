be9811b826504312dc47bf895e09132f8af128ed
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-07 15:57:28 +0800

    > make 221250067 w Linux wubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:28 up 34 min,  1 user,  load average: 0.99, 0.73, 0.74 2aefb4e124780c7510fcb448324d0aa6e4c371c

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ public class Main
         if (args.length < 1) {
             System.err.println("input path is required");
         }
-        //String source = args[0];
-        String source = "tests/test1.sysy";
+        String source = args[0];
+        //String source = "tests/test1.sysy";
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
 //        sysYLexer.removeErrorListeners();
@@ -22,8 +22,8 @@ public class Main
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
         SysYParser sysYParser = new SysYParser(tokens);
         sysYParser.removeErrorListeners();
-        ErrorListener myErrorListener = new ErrorListener();
-        sysYParser.addErrorListener(myErrorListener);
+        //ErrorListener myErrorListener = new ErrorListener();
+        //sysYParser.addErrorListener(myErrorListener);
         //SysYParser.ExpContext ctx = sysYParser.exp();
 
         ParseTree tree = sysYParser.program();
@@ -31,18 +31,18 @@ public class Main
 
 
 
-        if (myErrorListener.hasErrorInformation) {
-            // 假设myErrorListener有一个错误信息输出函数printLexerErrorInformation.
-            //myErrorListener.printLexerErrorInformation();
-            myErrorListener.printParserErrorInformation();
-        } else {
+//        if (myErrorListener.hasErrorInformation) {
+//            // 假设myErrorListener有一个错误信息输出函数printLexerErrorInformation.
+//            //myErrorListener.printLexerErrorInformation();
+//            myErrorListener.printParserErrorInformation();
+//        } else {
             MyVisitor visitor = new MyVisitor();
             visitor.visit(tree);
             //visitor.visitExp(ctx);
 //            for (Token t : tokens) {
 //                printSysYTokenInformation(t);
 //            }
-        }
+        //}
 
     }
 
